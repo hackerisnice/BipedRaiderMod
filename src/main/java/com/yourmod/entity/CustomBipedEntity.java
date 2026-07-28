03a5021879a54851021abae99d107e951ad0e001
hackerisnice <131273970+hackerisnice@users.noreply.github.com>
2026-07-28 18:54:56 +0800

    Update CustomBipedEntity.java

--- src/main/java/com/yourmod/entity/CustomBipedEntity.java
+++ src/main/java/com/yourmod/entity/CustomBipedEntity.java
@@ -20,6 +20,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
+import net.minecraft.core.HolderLookup;
 
 public class CustomBipedEntity extends PathfinderMob {
 
