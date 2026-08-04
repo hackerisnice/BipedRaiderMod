8791fec4ee7b0db7f035975f0c0dc73c5d365187
hackerisnice <131273970+hackerisnice@users.noreply.github.com>
2026-08-04 14:57:40 +0800

    Update BipedRaiderModClient.java

--- src/main/java/com/yourmod/BipedRaiderModClient.java
+++ src/main/java/com/yourmod/BipedRaiderModClient.java
@@ -2,13 +2,14 @@ package com.yourmod;
 
 import com.yourmod.client.renderer.CustomBipedRenderer;
 import com.yourmod.client.renderer.FriendlyBipedRenderer;
+import com.yourmod.registry.ModEntities; // 导入实体注册类
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 public class BipedRaiderModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistry.register(BipedRaiderMod.CUSTOM_BIPED, CustomBipedRenderer::new);
-        EntityRendererRegistry.register(BipedRaiderMod.FRIENDLY_BIPED, FriendlyBipedRenderer::new);
+        EntityRendererRegistry.register(ModEntities.CUSTOM_BIPED, CustomBipedRenderer::new);
+        EntityRendererRegistry.register(ModEntities.FRIENDLY_BIPED, FriendlyBipedRenderer::new);
     }
 }
