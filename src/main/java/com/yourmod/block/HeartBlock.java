0363b5b182af5bef5da1711e4af10b08a7c49ff8
hackerisnice <131273970+hackerisnice@users.noreply.github.com>
2026-08-04 14:52:01 +0800

    Update HeartBlock.java

--- src/main/java/com/yourmod/block/HeartBlock.java
+++ src/main/java/com/yourmod/block/HeartBlock.java
@@ -1,5 +1,6 @@
 package com.yourmod.block;
 
+import com.yourmod.util.IEntityDataSaver;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -15,11 +16,11 @@ public class HeartBlock extends Block {
         super(properties);
     }
 
-    // ★ 记录全局状态：当玩家放下这个方块时，将坐标深深烙印在玩家的数据里
     @Override
     public void setPlacedBy(Level level, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
         if (placer instanceof Player player) {
-            player.getPersistentData().putLong("PlacedHeartBlockPos", pos.asLong());
+            // ★ 修改为强转
+            ((IEntityDataSaver) player).getPersistentData().putLong("PlacedHeartBlockPos", pos.asLong());
         }
         super.setPlacedBy(level, pos, state, placer, stack);
     }
